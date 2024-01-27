67cb29c44c8bcffeefd19da2492c3163cf4a0709
sagar <sagartiwary360.com>
2024-01-27 12:25:03 +0530

    Router is created successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Profile from "./Pages/Profile";
 
 const App = () => {
-  return <div>
-    <h1 className="">Hey sagar how are you!</h1>
-  </div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
